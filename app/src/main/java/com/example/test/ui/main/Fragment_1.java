ae558e4fbd521be32c6c72555b41db3f3b3920e2
uwol1116 <62086770+uwol1116@users.noreply.github.com>
2020-05-29 02:04:45 +0900

    apply pull to refresh

--- app/src/main/java/com/example/test/ui/main/Fragment_1.java
+++ app/src/main/java/com/example/test/ui/main/Fragment_1.java
@@ -1,6 +1,10 @@
 package com.example.test.ui.main;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,7 +22,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.test.R;
+import com.github.mikephil.charting.charts.LineChart;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,8 +33,8 @@ import java.util.TimerTask;
 
 public class Fragment_1<stringRequest> extends Fragment {
     private TextView data;
-    private TimerTask mTimer;
-    RequestQueue queue;
+    private TextView text;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
 
     public static Fragment_1 newInstance(){
@@ -37,20 +44,9 @@ public class Fragment_1<stringRequest> extends Fragment {
 
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate((savedInstanceState));
-        queue = Volley.newRequestQueue(getActivity().getApplicationContext());
-        final String log = "11";
-
-        Timer timer = new Timer();
-
-        TimerTask TT = new TimerTask(){
-            public void run(){
-                Log.d("Tag","timer");
-                GetData();
-            }
-        };
-
-        timer.schedule(TT,0,1000);
+        GetData();
     }
+
 
     @Override
     public View onCreateView(
@@ -58,42 +54,22 @@ public class Fragment_1<stringRequest> extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_1, container, false);
         data = root.findViewById(R.id.textView);
+        text = root.findViewById(R.id.textView2);
+        LineChart lineChart = (LineChart)root.findViewById(R.id.chart);
 
-
-
-
-        /*
-        //요청 전송
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
-        String url ="http://junior.catsecurity.net/~yesqwe/imfine/getdata.php";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            float pm2 = jsonObject.getLong("pm2");
-                            data.setText(pm2 + "");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println("fail");
+            public void onRefresh() {
+                GetData();
+                mSwipeRefreshLayout.setRefreshing(false);
+
             }
         });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-        */
         return root;
     }
+
+
 
     public void GetData(){
         //요청 전송
@@ -108,8 +84,38 @@ public class Fragment_1<stringRequest> extends Fragment {
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         try {
+                            Log.d("getdata","success");
                             JSONObject jsonObject = new JSONObject(response);
-                            float pm2 = jsonObject.getLong("pm2");
+                            double pm2 = jsonObject.getDouble("pm2");
+                            data.setText(pm2 + "");
+                            //좋음
+                            if(pm2>= 0 && pm2 <= 15){
+                                data.setTextColor(Color.BLUE);
+                                text.setText("좋음");
+                                text.setTextColor(Color.BLUE);
+
+                            }
+                            //보통
+                            else if (pm2 >= 16 && pm2 <= 35){
+                                data.setTextColor(Color.GREEN);
+                                text.setText("보통");
+                                text.setTextColor(Color.BLUE);
+
+                            }
+                            //나쁨
+                            else if (pm2 >= 36 && pm2 <= 75){
+                                data.setTextColor(Color.YELLOW);
+                                text.setText("나쁨");
+                                text.setTextColor(Color.BLUE);
+
+
+                            }
+                            //매우 나쁨
+                            else if (pm2 >= 76 && pm2 <= 500){
+                                data.setTextColor(Color.RED);
+                                text.setText("매우 나쁨");
+                                text.setTextColor(Color.BLUE);
+                            }
                             data.setText(pm2 + "");
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -126,27 +132,43 @@ public class Fragment_1<stringRequest> extends Fragment {
         queue.add(stringRequest);
     }
 
-    //데이터 수신 리스너
-/*
-    Response.Listener<String> responseListener = new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            try {
-                JSONObject jsonObject = new JSONObject(response);
-                float pm2 = jsonObject.getLong("pm2");
-                data.setText(pm2 + "");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    };
-*/
-    //데이터 요청
-/*
-    GetdataRequest getdataRequest = new GetdataRequest(responseListener);
-    GetdataRequest queue = Volley.newRequestQueue(getActivity().getApplicationContext());
-    queue.add(getdataRequest);
-*/
+    //draw line chart
+    public void draw_chart(){
+        //요청 전송
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        String url ="http://junior.catsecurity.net/~yesqwe/imfine/getchart.php";
 
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        try {
+                            JSONArray jsonArray = new JSONArray("history");
+
+                            for(int i = 0; i < jsonArray.length(); i++){
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                float history_pm2 = jsonObject.getLong("pm2");
+                                String history_time = jsonObject.getString("time");
+                                Log.d("pm2",history_pm2+"");
+                                Log.d("time",history_time);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.d("error","error");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("fail");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
 
 }
